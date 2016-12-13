6705b7964b587adec79f2d626eaf9a3ec4f95700
Kevin Salazar <kskevin170@yahoo.com>
2016-12-13 15:11:35 -0800

    backend for services and photos

--- api/controllers/services.js
+++ api/controllers/services.js
@@ -19,7 +19,49 @@ function createService(request, response) {
   });
 }
 
+// GET
+function showService(request, response) {
+  var id = request.params.id;
+
+  Service.findById({_id: id}, function(error, service) {
+    if(error) response.json({message: 'Could not find service b/c:' + error});
+
+    response.json({service: service});
+  }).select('-__v');
+}
+
+// updateService
+function updateService(request, response) {
+  var id = request.params.id;
+
+  Service.findById({_id: id}, function(error, picture) {
+    if(error) response.json({message: 'Could not find service b/c:' + error});
+
+    if(request.body.title) picture.title = request.body.title;
+    if(request.body.description) picture.description = request.body.description;
+
+    service.save(function(error) {
+      if(error) response.json({messsage: 'Could not update service b/c:' + error});
+
+      response.json({message: 'Service successfully updated', service: service});
+    });
+  }).select('-__v');
+}
+
+// DELETE PICTURE
+function deleteService(request, response) {
+  var id = request.params.id;
+
+  Service.remove({_id: id}, function(error) {
+    if(error) response.json({message: 'Could not delete service b/c:' + error});
+
+    response.json({message: 'Service successfully deleted'});
+  }).select('-__v');
+}
 module.exports = {
   index: getAll,
   create: createService,
+  show: showService,
+  patch: updateService,
+  delete: deleteService
 }
--- api/routes/picture.js
+++ api/routes/picture.js
@@ -4,7 +4,20 @@ var router = express.Router();
 var pictureController = require('../controllers/pictures')
 
 router.route('/')
+// Get all Pictures
   .get(pictureController.index)
-  .post(pictureController.create)
+// Create new picture
+  .post(pictureController.create);
+
+router.route('/:id')
+  // GET return specific pictures
+  .get(pictureController.show)
+
+  // PATCH update existing picture
+  .patch(pictureController.patch)
+
+  // DELETE remove specific picture from DB
+  .delete(pictureController.delete);
+
 
 module.exports = router;
